1da4c72ae3c5702d818a15e73e51d769d19be65f
AlessandroRivera <00275220@uca.edu.sv>
2024-10-10 10:50:57 -0600

    feat: Funcion para mantener la propiedad del monticulo

--- Software/heapsort.hpp
+++ Software/heapsort.hpp
@@ -17,3 +17,22 @@ void MonticuloMax::construirMonticuloMax(Cliente *array, int tamano) {
     for (int i = (tamano / 2) - 1; i >= 0; i--) 
         maxHeapify(array, i, tamano); 
 } 
+
+// Funcion para mantener la propiedad del MonticuloMax
+void MonticuloMax::maxHeapify(Cliente *array, int i, int tamano) { 
+    int l = izquierda(i); 
+    int r = derecha(i); 
+    int mayor = i; 
+
+    // Compara el nodo actual con sus hijos para mantener el MonticuloMax
+    if (l < tamano && array[l].ingreso > array[i].ingreso) 
+        mayor = l; 
+    if (r < tamano && array[r].ingreso > array[mayor].ingreso) 
+        mayor = r; 
+    
+    // Si el mayor no es el nodo actual, intercambia y llama a maxHeapify de nuevo
+    if (mayor != i) { 
+        swap(array[i], array[mayor]); 
+        maxHeapify(array, mayor, tamano); 
+    } 
+} 
